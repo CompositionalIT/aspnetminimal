ea15ec2f81115380a389d178c56764c9e8a81033
Isaac Abraham <isaac.abraham@gmail.com>
2022-11-20 20:47:37 +0000

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+
+type Handler = System.Func<HttpContext, obj>
+
+let builder = WebApplication.CreateBuilder()
+let app = builder.Build()
+app.MapGet("/", Handler (fun ctx -> {| Message = "Hello, World as a JSON object!" |})) |> ignore
+app.MapGet("/text", Handler (fun ctx -> "Hello, World as a string!")) |> ignore
+app.Run ()
