22326d4be6bec39179d29a306382815c01821a96
Isaac Abraham <isaac.abraham@gmail.com>
2022-11-20 22:17:45 +0000

    Add Swagger support

--- Program.fs
+++ Program.fs
@@ -1,10 +1,23 @@
 ﻿open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
 
-type Handler = System.Func<HttpContext, obj>
+let builder =
+    let builder = WebApplication.CreateBuilder()
+    builder.Services.AddEndpointsApiExplorer().AddSwaggerGen() |> ignore
+    builder
 
-let builder = WebApplication.CreateBuilder()
+type Message = { Message : string }
+
 let app = builder.Build()
-app.MapGet("/", Handler (fun ctx -> {| Message = "Hello, World as a JSON object!" |})) |> ignore
-app.MapGet("/text", Handler (fun ctx -> "Hello, World as a string!")) |> ignore
-app.Run ()
+
+do
+    let handler f = System.Func<HttpContext, _> f
+    app.MapGet("/object", handler (fun ctx -> { Message = "Hello, World as a JSON object!" })) |> ignore
+    app.MapGet("/text", handler (fun ctx -> "Hello, World as a string!")) |> ignore
+
+
+do
+    app.UseSwagger().UseSwaggerUI() |> ignore
+
+app.Run()
