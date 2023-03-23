5f12a71416f5e4b7c2cbea4099acea111a5356c4
Isaac Abraham <isaac.abraham@gmail.com>
2023-03-23 23:04:37 +0000

    Test code

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
+open System.Threading.Tasks
 
 let app =
     let builder = WebApplication.CreateBuilder()
@@ -10,9 +11,19 @@ let app =
     app
 
 /// A helper function to convert F# lambdas to an .NET Func
-let handler f = System.Func<HttpContext, _> f
+let handler f = System.Func<_> f
+let handlerP f = System.Func<_,_> f
 
-app.MapGet("/object", handler (fun ctx -> {| Message = "Hello, World as a JSON object!" |})) |> ignore
-app.MapGet("/text", handler (fun ctx -> "Hello, World as a string!")) |> ignore
+type ObjectResponse = { Message : string }
+
+// app.MapGet("/object", System.Func<Unit,ObjectResponse> (fun () -> { Message = "Hello, World as a JSON object!" })) |> ignore
+
+
+
+app.MapGet("/text", handler (fun () -> "Hello, World as a string!")) |> ignore
+app.MapGet("/hello/{name}", System.Func<string, _> (fun (name:string) -> $"Greeting, {name.ToUpper()}!")) |> ignore
+
+type Input = { Name : string }
+app.MapPost("/hello", handlerP (fun (person:Input) -> task { return person.Name.ToUpper() })) |> ignore
 
 app.Run()
